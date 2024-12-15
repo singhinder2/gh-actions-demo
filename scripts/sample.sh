b6ea964f4a1ca96e8795efdd2af6495b6c372877
Inder Singh <60784160+singhinder2@users.noreply.github.com>
2024-12-16 00:26:11 +0530

    Update sample.sh

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+set -eo pipefail
+
 echo "GITHUB_REF_NAME=$GITHUB_REF_NAME"
 
 echo "REPO=$REPO"
 
 echo "REPO=$GITHUB_REPOSITORY"
+
+archive_name="$REPO"-"$GITHUB_REF_NAME"
+
+mkdir build
+
+cd build
+
+git archive --prefix "$REPO"/ -o "$archive_name".zip main
+git archive --prefix "$REPO"/ -o "$archive_name".tar.gz main
